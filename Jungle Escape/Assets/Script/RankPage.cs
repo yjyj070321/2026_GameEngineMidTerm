9ac40047a034c01a29b235b4a646f9ab00b4e84b
yjyj070321 <yjyj070321@gmail.com>
2026-05-12 21:40:34 +0900

    Main 화면의 랭킹 페이지를 Stage별로 구분해서 표시하는 기능

--- Jungle Escape/Assets/Script/RankPage.cs
+++ Jungle Escape/Assets/Script/RankPage.cs
@@ -1,28 +1,31 @@
-using System.Linq;
-using UnityEngine;
 using TMPro;
-
+using UnityEngine;
+using System.Linq;
 public class RankPage : MonoBehaviour
 {
-    [SerializeField] Transform contentRoot;
+
+    public int stagelevel = 1;
+
+   [SerializeField] Transform contentRoot;
+
     [SerializeField] GameObject rowPrefab;
 
     StageResultList allData;
 
     void Awake()
     {
-        allData = StageResultSaver. LoadRank();
+        allData = StageResultSaver.LoadRank();
         RefreshRankList();
     }
-
     void RefreshRankList()
+
     {
         foreach (Transform child in contentRoot)
         {
             Destroy(child.gameObject);
         }
-    
-        var sortedData = allData.results.Where(r => r.stage == 1).OrderByDescending(x => x.score).ToList();
+
+        var sortedData = allData.results.Where(r => r.stage == stagelevel).OrderByDescending(x => x.score).ToList();
 
         for (int i = 0; i < sortedData.Count; i++)
         {
@@ -30,5 +33,5 @@ public class RankPage : MonoBehaviour
             TMP_Text rankText = row.GetComponentInChildren<TMP_Text>();
             rankText.text = $"{i + 1}. {sortedData[i].playerName} - {sortedData[i].score}";
         }
-    }  
+    }
 }
